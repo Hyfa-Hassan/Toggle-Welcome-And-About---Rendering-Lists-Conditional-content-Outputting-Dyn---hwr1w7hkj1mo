3a156a763bed1246d0072693dc2d20471e0ee490
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-23 19:30:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
-
 const App = () => {
-  const [status,setStatus] = useState("welcome-div");
+  const [state, setState] = useState(true);
+  const handleToggle = () => {
+    if (state === true) {
+    setState(false);
+    } else {
+    setState(true);
+    }
+  };
   return (
-    <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle" onclick={()=>setStatus(status?"about-div":"welcome-div")}>Toggle</button>
-    </div>
+  <div id="main">{state === true ? (<div id="welcome-div">{<h1>Welcome to Newton School!!</h1>}</div>) : (<div id="about-div">
+   {
+    <h1>
+    Newton School is a neo-university providing highly immersive tech learning to millions of students, to tap into new-age tech opportunities.
+    </h1>
+  }
+  </div>
+  )}
+  <button id="toggle" onClick={handleToggle}>Toggle</button>
+  </div>
   );
 };
 
-export default App;
+ export default App;
